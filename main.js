12c16a9fa3c88a8ac9365196b5bfc8b2314f3856
AJ Cox <ajcox@ajs-macbook-pro.lan>
2023-01-17 21:28:23 -0700

    zones to publish

--- main.js
+++ main.js
@@ -6,16 +6,22 @@ let postEstimatedMaxHR = document.getElementById('estimatedHR')
 // getting access to local storage that was set up on function line 15
 let estimatedMaxHR = localStorage.getItem('EstimatedMXHR')
 
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+
+let curretZone = document.getElementById('currentZone')
 
 // function to get their estimated heart rate
 submit.addEventListener('click', function () {
     let userAge = document.getElementById('userAge').value
     let estimatedMaxHR = Math.round((208 - (.7 * userAge)))
     postEstimatedMaxHR.innerHTML = estimatedMaxHR + ' BPM'
+
+    let slider = document.getElementById("myRange");
+    slider.max = estimatedMaxHR
     // add user's heart rate to local storge so we can access it later on a global scope
     localStorage.setItem('EstimatedMXHR', estimatedMaxHR)
 })
-
 
 // function that will allow us to know what HR zone the user is in
 function usersHRZones(currentHR) {
@@ -25,17 +31,27 @@ function usersHRZones(currentHR) {
     // Zone 4	Hard	80–90%
     // Zone 5	Maximum	90–100%
     if (currentHR <= (estimatedMaxHR * .6)) {
-        console.log('Zone 1')
+        curretZone.innerHTML = 'Zone 1'
     } else if (currentHR <= (estimatedMaxHR * .7)) {
-        console.log('Zone 2')
+        curretZone.innerHTML = 'Zone 2'
     } else if (currentHR <= (estimatedMaxHR * .8)) {
-        console.log('Zone 3')
+        curretZone.innerHTML = 'Zone 3'
     } else if (currentHR <= (estimatedMaxHR * .9)) {
-        console.log('Zone 4')
+        curretZone.innerHTML = 'Zone 4'
     } else {
-        console.log('Zone 5')
+        curretZone.innerHTML = 'Zone 5'
     }
-
 }
 
-usersHRZones(175)
+
+
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
+
+slider.addEventListener('mouseup', (e) => {
+    usersHRZones(slider.value)
+});
